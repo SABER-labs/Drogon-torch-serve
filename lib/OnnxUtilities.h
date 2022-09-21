0bf71d6685b420cae969425fec22e85a00f6ab2b
Arjun Variar <Arjun Variar>
2022-09-21 13:33:58 -0400

    Improving memory access.

--- lib/OnnxUtilities.h
+++ lib/OnnxUtilities.h
@@ -17,8 +17,8 @@ std::tuple<float, std::string> getTopResult(std::vector<float>::iterator, std::v
 
 Ort::Session createOrtSession(const std::string&);
 
-std::tuple<std::vector<float>, std::vector<float>, std::vector<int64_t>, std::vector<int64_t>> generateInputOutputTensorValuesForORT(std::vector<cv::Mat>&, int64_t);
+std::tuple<std::vector<float>, std::vector<float>, std::vector<int64_t>, std::vector<int64_t>> generateInputOutputTensorValuesForORT(std::vector<std::reference_wrapper<const cv::Mat>>&, int64_t);
 
-cv::Mat processImage(const cv::Mat&);
+cv::Mat processImage(std::reference_wrapper<const cv::Mat>);
 
 uint getNumInferenceEngineThreads();
